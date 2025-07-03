807e9af8095ebdf42bd6084adbaed35f4ca21806
santiagomr2708 <113460466+santiagomr2708@users.noreply.github.com>
2025-07-02 23:16:24 -0500

    cambios

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Página cargada correctamente.");
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', () => {
+  const expanded = menuToggle.getAttribute('aria-expanded') === 'true' || false;
+  menuToggle.setAttribute('aria-expanded', !expanded);
+  navLinks.classList.toggle('open');
+});
